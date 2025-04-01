af0f543fdbe89ae79c6b09e300e47801df0869ae
Kamil Szczepanowski <kamil.szczepanowski.external@atos.net>
2025-04-01 11:52:08 +0200

    TerraformZadanie1

---
+++ Zadanie1/zadanie1.tf
@@ -0,0 +1,19 @@
+terraform {
+    required_providers {
+      azurerm = {
+        source = "hashicorp/azurerm"
+        version = "~>3.94"
+      }
+    }
+}
+
+provider "azurerm" {
+    features {}
+}
+
+resource "azurerm_resource_group" "resource_group" {
+    name        = "TerraformTraining-A870719"
+    location    = "West Europe"
+  
+}
+
